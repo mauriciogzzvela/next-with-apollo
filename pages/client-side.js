2a7c3da12a3e3c04f72a398133e9d3dfa9a9ae65
Mauricio Vela <mauriciovela@MacBook-Pro-de-Mauricio.local>
2021-08-01 23:32:18 -0500

    bugfix, adding settings and prepare to deployment

--- pages/client-side.js
+++ pages/client-side.js
@@ -2,19 +2,49 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import ClientOnly from "../components/ClientOnly";
 import Countries from "../components/Countries";
+import Image from "next/image";
 
 export default function ClientSide() {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Create Next App</title>
+                <title>Next.js with Apollo Client App</title>
+                <meta name="description" content="Next.js with Apollo App" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}><a href="#welcome-to-next-js" aria-hidden="true" class="aal_anchor" id="welcome-to-next-js"><svg aria-hidden="true" class="aal_svg" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
+                <h1 className={styles.title}>
+                    Next.js with Apollo Client App
                 </h1>
+
+                <div>
+                    <p className={styles.description}>  </p>
+                    <code className={styles.code}>
+                        <a href={"https://github.com/mauriciogzzvela"}
+                           target="_blank"
+                           rel="noreferrer"
+                           className={styles.verticalAlignMiddle}
+                        >
+                            <span className={styles.logo}>
+                                <Image src="/GitHub-Mark-64px.png" alt="Github Logo" width={32} height={32} />
+                            </span>
+                            <span>@mauriciogzzvela</span>
+                        </a>
+                    </code>
+                </div>
+
+                <p className={styles.descriptionPage}>
+                    Client Side Rendering Page
+                </p>
+
+                <p className={styles.linkPage}>
+                    <a href={"/"}>Static Rendering Page</a>
+                </p>
+
+                <p className={styles.linkPage}>
+                    <a href={"/server-side"}>Server Side Rendering Page</a>
+                </p>
                 <ClientOnly>
                     <Countries />
                 </ClientOnly>
@@ -27,7 +57,7 @@ export default function ClientSide() {
                     rel="noopener noreferrer"
                 >
                     Powered by{" "}
-                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+                    <Image src="/vercel.svg" alt="Vercel Logo" className={styles.logo} width={72} height={16} />
                 </a>
             </footer>
         </div>
